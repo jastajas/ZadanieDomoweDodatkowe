bb3ddc24b4b3f9648105440720276f1ef0579252
jackS <jastajas@gamil.com>
2018-01-16 20:13:04 +0100

    Wersja 1 - procesor

---
+++ src/Procesor.java
@@ -0,0 +1,14 @@
+public class Procesor {
+
+    String producer;
+    String model;
+    int clock;
+
+    Procesor(String producer,String model, int clock){
+        this.producer = producer;
+        this.model = model;
+        this.clock = clock;
+    }
+
+
+}
