7c196232d30177c9046649e5b85c75ccd14ad278
jackS <jastajas@gamil.com>
2018-01-17 23:13:39 +0100

    Nowa klasa ComputerAssembly

---
+++ src/ComputerAssembly.java
@@ -0,0 +1,11 @@
+public class ComputerAssembly {
+    Computer computer;
+
+
+    // public ComputerAssembly(Computer computer){
+   //}
+
+    Computer assembly(Computer computer){
+        return this.computer = computer;
+    }
+}
