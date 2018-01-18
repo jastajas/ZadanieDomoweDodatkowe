0ab84e6cd84190cd42e73bfb4949acb25a8e3947
jackS <jastajas@gamil.com>
2018-01-18 17:05:58 +0100

    zmodyfikowana metoda assebly ComputerAssembly

--- src/ComputerAssembly.java
+++ src/ComputerAssembly.java
@@ -1,11 +1,20 @@
 public class ComputerAssembly {
-    Computer computer;
+    //Computer computer;
 
 
     // public ComputerAssembly(Computer computer){
    //}
 
-    Computer assembly(Computer computer){
-        return this.computer = computer;
+    Computer assembly(String computerName, String procesorName, String porcesorModel, int procesorClock, String memoryName, String memoryModel, int memoryClocko, int memorySize){
+        return new Computer(computerName,new Procesor(procesorName,porcesorModel,procesorClock),new Memory(memoryName,memoryModel,memoryClocko,memorySize));
     }
+    Computer assemblyShort(String computerName, Procesor procesor, Memory memory){
+        return new Computer(computerName, procesor, memory);
+    }
+
+   // Computer assembly(Computer computer){
+
+        //return
+                //this.computer = computer;
+   // }
 }
